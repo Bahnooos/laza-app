e35f7bae7c31549419cbf426d37fd704aded8d82
Bahnas <bahnas114@gmail.com>
2025-10-07 09:34:42 +0300

    Add AppColor class with predefined color constants

---
+++ lib/core/theme/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color mainBlueColor = Color(0xff9775FA);
+  static const Color orangeColor = Color(0xffFF7043);
+  static const Color darkGrayColor = Color(0xFF1D1E20);
+
+  static const Color gray = Color(0xFF8F959E);
+
+  static const Color ghostWhite = Color(0xFFF5F6FA);
+
+  static const Color white = Color(0xFFFFFFFF);
+}
